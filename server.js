062e639feb539bec955177d441f388008501557f
maxiconstancio <maxiconstancio@gmail.com>
2021-12-18 12:36:41 -0300

    header parser completed

--- server.js
+++ server.js
@@ -21,8 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let ipaddress = req.headers["x-forwarded-for"];
+  let language = req.headers["accept-language"] 
+  let software = req.headers["user-agent"];
+
+
+  res.json({ipaddress, language, software});
 });
 
 
